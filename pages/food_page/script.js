48c9d1a44e0fb41645744ced5c276834763be356
aniket <aniket.joshi@utexas.edu>
2019-01-27 02:11:38 -0600

    improving food page

--- pages/food_page/script.js
+++ pages/food_page/script.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 
 window.onload=function() {
 
@@ -10,9 +11,18 @@ function AddToShoppingCart(name, type) {
         M.toast({html: 'Cannot add multiple items of the same type!', displayLength:2000});
     } else {
         items[type] = name;
-        var x = document.getElementById("cartbutton");
+        var x = document.getElementById("cartspan");
         var n = parseInt(x.innerText.charAt(0)) + 1;
         x.innerText = n + x.innerText.substring(1, x.innerText.length);
+        M.toast({html: 'Added ' + name + ' to shopping cart!', displayLength:2000});
     }
+}
 
+function toShoppingCart() {
+    var stream = fs.createWriteStream('data/foodPersistence.json');
+    stream.once('open', function (fd) {
+        stream.write(JSON.stringify(items));
+        stream.end()
+    });
+    transfer('cart_page');
 }
