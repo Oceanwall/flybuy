9b8eeba083fbb7656efebb07ffd2009c835ec681
aniket <aniket.joshi@utexas.edu>
2019-01-26 23:47:47 -0600

    improving food page

--- pages/food_page/script.js
+++ pages/food_page/script.js
@@ -1,1 +1,18 @@
-let imagesPath = '../../images/';
+
+window.onload=function() {
+
+};
+
+var items = {};
+
+function AddToShoppingCart(name, type) {
+    if (type in items) {
+        M.toast({html: 'Cannot add multiple items of the same type!', displayLength:2000});
+    } else {
+        items[type] = name;
+        var x = document.getElementById("cartbutton");
+        var n = parseInt(x.innerText.charAt(0)) + 1;
+        x.innerText = n + x.innerText.substring(1, x.innerText.length);
+    }
+
+}
