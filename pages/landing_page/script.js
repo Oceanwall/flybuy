304bc76d8cf87547fadf24d3a0cc5b87288a9c4a
aniket <aniket.joshi@utexas.edu>
2019-01-27 09:19:06 -0600

    landing page translation

--- pages/landing_page/script.js
+++ pages/landing_page/script.js
@@ -1,13 +1,19 @@
 const swal = require('sweetalert');
 const fs = require('fs');
 
+
+const request = require('request');
+const uuidv4 = require('uuid/v4');
+
+
 window.onload = function() {
 
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems);
 
   document.getElementById("selecto").onchange = function() {
-    console.log(document.getElementById("selecto").value);
+    translate(document.getElementById("selecto").value);
+    //console.log(document.getElementById("selecto").value);
   }
 
   let clicked = false;
@@ -57,6 +63,7 @@ window.onload = function() {
     document.getElementById("nameTitle").innerHTML = 'Hello, ' + data.toString().split('\n')[0];
   });
 
+ // translate("fr");
 }
 
 function eventFire(el, etype){
@@ -66,5 +73,58 @@ function eventFire(el, etype){
     var evObj = document.createEvent('Events');
     evObj.initEvent(etype, true, false);
     el.dispatchEvent(evObj);
+  }
+}
+
+var toTranslate = ["Hello", "Menu", "Flight Information", "Customer Feedback", "Call Attendant"];
+
+function translate(language) {
+  const subscriptionKey = "be3d29f1a40146c8801bd071c607c795";
+  if (!subscriptionKey) {
+    throw new Error('Environment variable for your subscription key is not set.')
+  };
+
+  function translateText(text, i){
+    let options = {
+      method: 'POST',
+      baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+      url: 'translate',
+      qs: {
+        'api-version': '3.0',
+        'to': language
+      },
+      headers: {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': uuidv4().toString()
+      },
+      body: [{
+        'text': text
+      }],
+      json: true,
+    };
+
+    request(options, function(err, res, body){
+      if (i == 0) {
+        fs.readFile('data/persistence.txt', 'utf8', function (err, data) {
+          if (err) {
+            console.log(err);
+          }
+          document.getElementById("nameTitle").innerHTML = body[0]["translations"][0]["text"] + ', ' + data.toString().split('\n')[0];
+        });
+      } else if (i==1) {
+        document.getElementById("menu").innerHTML = body[0]["translations"][0]["text"];
+      } else if(i==2) {
+        document.getElementById("plane").innerHTML = body[0]["translations"][0]["text"];
+      } else if (i==3){
+        document.getElementById("customer").innerHTML = body[0]["translations"][0]["text"];
+      } else {
+        document.getElementById("request").innerHTML = body[0]["translations"][0]["text"];
+      }
+    });
+  };
+  var i = 0;
+  while(i < 5) {
+    translateText(toTranslate[i], i++);
   }
 }
